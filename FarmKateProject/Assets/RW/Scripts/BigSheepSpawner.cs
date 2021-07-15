c18e8544c19b1f31b97f7631f39c18fbdb7e8f58
Kate4321 <mka.2005.k@gmail.com>
2021-07-15 17:48:47 +0300

    BigSheep

--- FarmKateProject/Assets/RW/Scripts/BigSheepSpawner.cs
+++ FarmKateProject/Assets/RW/Scripts/BigSheepSpawner.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepSpawner : MonoBehaviour
+public class BigSheepSpawner : MonoBehaviour
 {
-    [Header("Sheep1")]
-    [SerializeField] private GameObject sheep1Prefab;
-    [SerializeField] private Vector3 spawnPosition1;
-    [SerializeField] private Vector3 spawnPosition2;
-    [SerializeField] private Vector3 spawnPosition3;
-
-    [Header("Sheep")]
+    [Header("BigSheep")]
     [SerializeField] private GameObject sheepPrefab;//префаб овцы
     [SerializeField] private Vector3 spawnPosition;//позиция спауна
-    [SerializeField] private Vector2 xSpawnBounds;//границы(рандомная точка в этом диапазоне)
+    [SerializeField] private Vector3 xSpawnBounds;//границы(рандомная точка в этом диапазоне)
 
     [SerializeField] private int sheepCount;
     [SerializeField] private float spawnRate;//частота появления между овцами
@@ -43,12 +37,11 @@ public class SheepSpawner : MonoBehaviour
     private void CreateSheep()
     {
         //22 - -22, 0, 55;
-        float xRandomPosition = Random.Range(xSpawnBounds.x, xSpawnBounds.y);//найти рандомную позицию по х
+        float xRandomPosition = Random.Range(xSpawnBounds.x, xSpawnBounds.y, xSpawnBounds.z);//найти рандомную позицию по х
         Vector3 randomSpawnPosition = new Vector3(xRandomPosition, spawnPosition.y, spawnPosition.z);//сформировать новую позицию
         Instantiate(sheepPrefab, randomSpawnPosition, sheepPrefab.transform.rotation);
         //new Vector3(Random.Range(xSpawnBounds.x, xSpawnBounds.y), spawnPosition.y, spawnPosition.z)
 
 
     }
-    
 }
