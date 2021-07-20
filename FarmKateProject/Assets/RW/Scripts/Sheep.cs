bf15f0e4cf6427ae7f3dcf36bdeac7196443702b
Kate4321 <86675857+Kate2005@users.noreply.github.com>
2021-07-20 16:07:27 +0300

    а

--- FarmKateProject/Assets/RW/Scripts/Sheep.cs
+++ FarmKateProject/Assets/RW/Scripts/Sheep.cs
@@ -6,19 +6,25 @@ public class Sheep: MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private Vector3 moveDirection;
-    [SerializeField] private float force;
+    [SerializeField] public float jumpSpeed;
+    [SerializeField] public float gravity;
+    [SerializeField] private Vector3 jumpDirection;
+   [SerializeField] private float force;
     private Rigidbody rb;
+    //private BoxCollider bc;
     [SerializeField] private GameObject particlesPrefab;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        //bc  = GetComponent<Collider>();
     }
 
 
     void Update()
     {
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
     }
     public void SaveSheep()
     {
@@ -26,13 +32,15 @@ public class Sheep: MonoBehaviour
         
             rb.isKinematic = false;
             rb.AddForce(Vector3.up * force);
-            ///////////////////////// тут делать
-            //1. Отключить скорость овце на 0 или как в тракторе
-            //2. отключить бокс коллайдер овце
-            //3. отключить гравитацию
-            //4. Спаунить патикл со здвигом над овцой или за овцой
-            //спауним патикл в позиции овцы
-            GameObject particle = Instantiate(particlesPrefab, transform.position, particlesPrefab.transform.rotation); //senoPrefab.transform.rotation
+        
+            //GetComponent<bc>();
+            //enabled = false;
+        //1. Отключить скорость овце на 0 или как в тракторе
+        //2. отключить бокс коллайдер овце
+        //3. отключить гравитацию
+        //4. Спаунить патикл со здвигом над овцой или за овцой
+        //спауним патикл в позиции овцы
+        GameObject particle = Instantiate(particlesPrefab, transform.position, particlesPrefab.transform.rotation); //senoPrefab.transform.rotation
             Destroy(particle, 2f);
             Destroy(gameObject, 0.9f);
         
